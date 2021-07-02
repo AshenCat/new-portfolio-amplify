d2365e4f089cae164a5c9bba5c5a910fc46b34fe
ashencat <forddagujar@gmail.com>
2021-07-02 13:46:38 -0400

    gif and webp declarations

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -6,3 +6,13 @@ declare module '*.jpg' {
     const value: any;
     export default value;
 }
+
+declare module '*.webp' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.gif' {
+    const value: any;
+    export default value;
+}
