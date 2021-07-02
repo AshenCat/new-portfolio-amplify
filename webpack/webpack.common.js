507fe0b0bfbae49d5dfbc567a00bb7eb370a5bab
ashencat <forddagujar@gmail.com>
2021-07-02 13:47:17 -0400

    webp webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -28,6 +28,10 @@ module.exports = {
             {
                 test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
                 type: 'asset/inline',
+            },
+            {
+                test: /\.(webp)$/i,
+                type: 'asset/inline',
             }
         ],
     },
