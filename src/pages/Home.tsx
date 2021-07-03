993b129daa310a1a05c2215b1ae7258084904303
ashencat <forddagujar@gmail.com>
2021-07-03 10:51:21 -0400

    footer on home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import Intro from './home/intro/Intro'
 import './Home.scss'
 import Projects from './home/projects/Projects';
 import Skills from './home/skills/Skills';
+import Footer from './home/zfooter/Footer';
 
 const Home: React.FunctionComponent<IPage & RouteComponentProps<any>> = () => {
     return <main className="home">
@@ -12,6 +13,7 @@ const Home: React.FunctionComponent<IPage & RouteComponentProps<any>> = () => {
         <Intro name="intro" />
         <Projects name="projects" />
         <Skills name="skills" />
+        <Footer name="footer" />
     </main>
 }
 
