de2d4bbf917880a5145192bf1c670f542b3e8e87
ashencat <forddagujar@gmail.com>
2021-07-21 18:16:47 -0400

    moved home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,11 @@
 import { RouteComponentProps, withRouter } from 'react-router-dom';
-import IPage from '../interfaces/page'
-import Hero from './home/hero/Hero'
-import Intro from './home/intro/Intro'
+import IPage from '../../interfaces/page'
+import Hero from './hero/Hero'
+import Intro from './intro/Intro'
 import './Home.scss'
-import Projects from './home/projects/Projects';
-import Skills from './home/skills/Skills';
-import Footer from './home/zfooter/Footer';
+import Projects from './projects/Projects';
+import Skills from './skills/Skills';
+import Footer from './zfooter/Footer';
 
 const Home: React.FunctionComponent<IPage & RouteComponentProps<any>> = () => {
     return <main className="home">
