18a15e9564bcf33e00fa0982b5113b3a1273016c
ashencat <forddagujar@gmail.com>
2021-08-01 17:33:36 -0400

    blog transition

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,12 +1,30 @@
+import { motion } from 'framer-motion';
 import React from 'react'
 import { RouteChildrenProps } from 'react-router-dom';
 import IPage from '../../interfaces/page';
+import './blog.scss'
 
 const Blog: React.FunctionComponent<IPage & RouteChildrenProps<any>> = () =>  {
+    const mainvariant = {
+        initial: {
+            x: '100vw', 
+        },
+        animate: {
+            x: 0, 
+        },
+    }
     return (
-        <div>
-            
-        </div>
+        <motion.main
+            variants={mainvariant}
+            initial="initial"
+            animate="animate"
+            className="blog"
+            transition={{
+                ease: 'easeIn'
+            }}
+        >
+            Under Construction
+        </motion.main>
     )
 }
 
